3f97a2f05da43a46324ca8006f610ca3b87d16d3
AVI SOLAR <avisolar@Ashokas-MacBook-Air.local>
2018-08-06 18:04:22 +0530

    add: update

--- node-js-promise/example-2.js
+++ node-js-promise/example-2.js
@@ -1,6 +1,15 @@
 //Using Fetch
 const fetch = require('node-fetch')
-
-fetch('https://jsonplaceholder.typicode.com/todos/1')
+let url1 = 'https://jsonplaceholder.typicode.com/todos/1' ;
+let url2 = 'https://jsonplaceholder.typicode.com/todos/2' ;
+fetch(url1)
   .then(response => response.json())
-  .then(json => console.log(json))
+  .then(json => {
+     console.log(json)
+     return fetch(url2)
+  })
+  .then(res => res.json())
+  .then(j => console.log(j))
+  .catch(err => console.log(err))
+
+
