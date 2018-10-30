bb2726f15446ec0856a284fdd58085f5f4e53ca2
Anish George <anishgeorge2690@gmail.com>
2018-10-30 16:20:23 +0530

    Find Index of array of objects

---
+++ findIndexOfArrayObject.js
@@ -0,0 +1,14 @@
+var temp = [
+    {
+        text: 'home',
+        name: 'dfdd'
+    },
+    {
+        text: 'home 123',
+        name: 'dfdf'
+    },
+]
+
+temp
+var index = temp.findIndex(x => x.text == 'home')
+index
